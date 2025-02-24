19b40858303e99609ac6e47f2b0ed84d0a534cd6
unknown <amirreza.asari2@gmail.com>
2025-02-24 14:53:39 +0330

    fix: minor issues

--- apps/reservations/src/reservations.repository.ts
+++ apps/reservations/src/reservations.repository.ts
@@ -1,12 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { AbstractRepository } from '@app/common/database/abstract.repository';
-import { AbstractDocument } from '@app/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ReservationDocument } from './models/reservation.schema';
 import { Model } from 'mongoose';
 
 @Injectable()
-export class ReservationsRepository extends AbstractRepository<AbstractDocument> {
+export class ReservationsRepository extends AbstractRepository<ReservationDocument> {
   protected readonly logger = new Logger(ReservationsRepository.name);
 
   constructor(
